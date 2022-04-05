617e8e4d352b457bdc25a34b8c3add2368e7ea79
Tal Hayut <talha@accessibe.com>
2022-04-05 15:58:29 +0300

    A

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ const { exec } = require('@actions/exec');
 
 async function run() {
   try {
-    const { pusher: { email, name } } = github.context.payload;
+    const { payload } = github.context;
+    const args = {};
+
+    // get input credentials
+    const inputUser = core.getInput('user');
+    const inputEmail = core.getInput('email');
+
+    if (payload.head_commit) {
+      args.email = payload.head_commit.committer.email;
+      args.username = payload.head_commit.committer.username;
+      args.message = payload.head_commit.message;
+    }
+
+    const { email, username } = args;
+
+    const userName = inputUser || username;
+    const userEmail = inputEmail || email;
+
+    if (!userName || !userEmail) {
+      const errorMessage = `failed to extract username or email from github context, please provide 'username' and 'email' through the workflow file.`;
+      return core.setFailed(errorMessage);
+    }
 
     // get input
     const destinationDir = core.getInput('dir');
@@ -20,14 +41,13 @@ async function run() {
 
     // generate docs
     await exec('npx', docsArgs);
-    
-    // push dist
-    await exec('git', ['config', '--local', 'user.name', name]);
-    await exec('git', ['config', '--local', 'user.email', email]);
-    await exec('git', ['add', '.']);
-    await exec('git', ['commit', '-a', '-m',  commitMsg]);
-    await exec('git', ['push', 'origin', `HEAD:${destBranch}`]);
 
+    // push dist
+    await exec('git', ['config', '--local', 'user.name', userName]);
+    await exec('git', ['config', '--local', 'user.email', userEmail]);
+    await exec('git', ['add', '.']);
+    await exec('git', ['commit', '-a', '-m', commitMsg]);
+    await exec('git', ['push', 'origin', `HEAD:${destBranch}`]);
   } catch (error) {
     core.setFailed(`Failed to publish ${error.message}`);
   }
